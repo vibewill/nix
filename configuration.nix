499895ac659616c434527ac7b2d72322715d3d6f
vibewill <vibewill@gmail.com>
2024-02-13 18:04:09 -0300

    flatpak

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,11 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   programs.hyprland.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  services.flatpak.enable = true;
+
   # Enable networking
   networking.networkmanager.enable = true;
   security.rtkit.enable = true;
